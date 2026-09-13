0116454634b614d7db1681fe3f62c4b048a9cdde
muhammad-tamim <contact2tamim@gmail.com>
2026-09-13 21:24:31 +0600

    feat: add technology grid for cards

--- src/components/Technologies/TechnologyGrid.tsx
+++ src/components/Technologies/TechnologyGrid.tsx
@@ -1,6 +1,35 @@
+import type { Technology } from "../../types/technology";
+import TechnologyCard from "./TechnologyCard";
 
-export default function TechnologyGrid() {
-    return (
-        <div>TechnologyGrid</div>
-    )
+interface TechnologyGridProps {
+    technologies: Technology[];
+    selectedTechnologies: Technology[];
+    onAddToStack: (technology: Technology) => void;
 }
+
+const TechnologyGrid = ({
+    technologies,
+    selectedTechnologies,
+    onAddToStack,
+}: TechnologyGridProps) => {
+    return (
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
+            {technologies.map((technology) => {
+                const isAdded = selectedTechnologies.some(
+                    (selectedTechnology) => selectedTechnology.id === technology.id
+                );
+
+                return (
+                    <TechnologyCard
+                        key={technology.id}
+                        technology={technology}
+                        isAdded={isAdded}
+                        onAddToStack={onAddToStack}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default TechnologyGrid;
